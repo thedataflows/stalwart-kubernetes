2b42554cacb020d84449a49b935c2c7edb9acdf4
Cristian Chiru <cristi.chiru@gmail.com>
2023-11-27 22:36:59 +0200

    rename secrets

--- gitops.sh
+++ gitops.sh
@@ -1,18 +1,28 @@
 #!/bin/env bash
 
-set -e -o pipefail -u -x
+set -e -o pipefail -u
 
 cd "${0%/*}"
 
+set -x
 OUTPUT_DIR=gitops
 
 rm -fr "$OUTPUT_DIR"
 mkdir -p "$OUTPUT_DIR"
-find out/ -type f -name 'v1_secret_*.yaml' -exec cp -v {} "$OUTPUT_DIR" \;
-cp -r config/ "$OUTPUT_DIR"
+{ set +x; } 2>/dev/null
+while IFS= read -r -d '' f; do
+  set -x
+  cp -v "out/$f" "$OUTPUT_DIR/${f/v1_secret_/secret.}"
+  { set +x; } 2>/dev/null
+done < <(cd out/; find . -type f -name 'v1_secret_*.yaml' -print0)
+set -x
+cp -vr config/ "$OUTPUT_DIR"
 sed -E 's,^# ,,g' templates/kustomization.yaml > "$OUTPUT_DIR/kustomization.yaml"
 cd "$OUTPUT_DIR"
-SECRETS=$(find -type f -name 'v1_secret_*.yaml' -printf '"%f",')
+{ set +x; } 2>/dev/null
+mapfile -t FILES < <(find . -type f -name 'secret.*.yaml' -printf '%f\n')
+printf -v SECRETS '"%s",' "${FILES[@]}"
+set -x
 yq -i '.resources=["https://github.com/thedataflows/stalwart-kubernetes?ref=latest",'$SECRETS'"config/"]' kustomization.yaml
 yq -i 'del(.secretGenerator)' config/kustomization.yaml
 rm -fr config/data config/etc/dkim
